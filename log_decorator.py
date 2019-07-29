9c397d9cb44f5b603cc9cab504a7b1c5ee4e789b
swardhan <saumyawardhan1@gmail.com>
2019-07-29 17:10:19 +0530

    Add Masking option

--- log_decorator.py
+++ log_decorator.py
@@ -3,39 +3,50 @@ import sys
 import datetime
 import logging
 
-logging.basicConfig(filename="newfile.log", format='%(asctime)s-%(message)s', datefmt="%b %d %Y-%H:%M:%S") 
+logging.basicConfig(filename="logfile.log", format='%(asctime)s-%(message)s', datefmt="%b %d %Y-%H:%M:%S") 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG) 
 
-def log(function):
+def log(mask_args=[], masked_kwargs=[]):
+	def outer_wrapper(function):
 
-	log_template = """
-%(func_name)s %(args)s %(kwargs)s %(ret_val)s %(traceback)s
-	"""
+		log_template = """
+	%(func_name)s %(args)s %(kwargs)s %(ret_val)s %(traceback)s
+		"""
 
-	def wrapper(*args, **kwargs):
+		def wrapper(*args, **kwargs):
 
-		ret_val = None
-		tb = None
-		dt = datetime.datetime.now()
+			ret_val = None
+			tb = None
+			dt = datetime.datetime.now()
+			temp_args = list(args)
+			temp_kwargs = kwargs
 
-		try:
-			ret_val = function(*args, **kwargs)
-		except Exception as e:
-			trace = sys.exc_info()
-			tb = '\n'.join(traceback.format_exception(*trace))
-			if tb is not None:
-				tb = '\n'+tb
+			for index in mask_args:
+				if index > 0 or index < len(mask_args):
+					temp_args[index] = 'xxxx'
 
-		finally:
-			log_data = log_template % {
-					'func_name': function.__name__,
-					'args': args,
-					'kwargs': kwargs,
-					'ret_val': ret_val,
-					'traceback': tb
-				}
+			for key in masked_kwargs:
+				temp_kwargs[key] = 'xxxx'
 
-			logger.info(log_data)
+			try:
+				ret_val = function(*args, **kwargs)
+			except Exception as e:
+				trace = sys.exc_info()
+				tb = '\n'.join(traceback.format_exception(*trace))
+				if tb is not None:
+					tb = '\n'+tb
 
-	return wrapper
+			finally:
+				log_data = log_template % {
+						'func_name': function.__name__,
+						'args': temp_args,
+						'kwargs': temp_kwargs,
+						'ret_val': ret_val,
+						'traceback': tb
+					}
+
+				logger.info(log_data)
+
+		return wrapper
+	return outer_wrapper
--- test.py
+++ test.py
@@ -1,15 +1,17 @@
 from log_decorator import log
 
-@log
+@log()
 def printer(arg):
 	pass
 
-@log
-def add(*args):
+@log()
+def add(*args, **kwargs):
 	total = 0
 	for arg in args:
 		total = total + arg
 	return total
 
-printer('asd', 1)
-add(1,2,3,4,5,6,7,8,9,10)
+printer('Saumya')
+
+add(1,2,3, abc="abc", test=11)
+
